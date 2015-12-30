fe302b99e5a819d57907a1704e32aadf68024253
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-12-29 21:11:30 -0500

    Restructure project

---
+++ lib/moviepilot/interface.rb
@@ -0,0 +1,66 @@
+module Moviepilot
+  class Interface
+
+    TAGS = %w(superheroes horror young-adult tv latest)
+
+    def initialize
+      @pastel_print = PastelPrint.new
+    end
+
+    def run
+      show_welcome_screen
+      show_main_menu
+    end
+
+    def show_welcome_screen
+      @pastel_print.title('Welcome to MoviePilot Client')
+    end
+
+    def show_main_menu
+      @pastel_print.title('Main Menu')
+      choose do |menu|
+        menu.prompt = "Please enter a number:"
+        menu.choice("Read Articles") { show_tags_menu }
+        menu.choice("Search") { puts "search menu"}
+        menu.choice("Exit") {}
+      end
+    end
+
+    def show_tags_menu
+      @pastel_print.title('Select a tag')
+      tag = choose do |menu|
+        menu.prompt = "Please enter a number:"
+        TAGS.each{ |e| menu.choice(e) }
+      end
+      articles = Gateway.trending_articles(tag)
+      show_article_list(articles)
+    end
+
+    def show_article_list(articles)
+      articles.each do |article|
+        puts "#{article['id']} - #{article['title']}"
+      end
+      pick_article_from_list(articles)
+    end
+
+    def pick_article_from_list(articles)
+      valid_ids = articles.map{|article| article['id']}
+      id_str = ask "Please enter the ID of one Article"
+      id = id_str.to_i
+      if valid_ids.include?(id)
+        show_article("post", id)
+      else
+        @pastel_print.alert "Invalid ID, try again..."
+        pick_article_from_list(articles)
+      end
+    end
+
+    def show_article(type, article_id)
+      article = Gateway.get_article("post", article_id)
+      puts "title: #{article[:title]}"
+      puts ReverseMarkdown.convert(article[:body])
+      show_main_menu
+    end
+
+  end
+end
--- lib/moviepilot/pastel_print.rb
+++ lib/moviepilot/pastel_print.rb
@@ -1,17 +1,17 @@
-require 'pastel'
+module Moviepilot
+  class PastelPrint
 
-class PastelPrint
+    def initialize
+      @pastel = Pastel.new
+    end
 
-  def initialize
-    @pastel = Pastel.new
+    def title(text)
+      puts @pastel.decorate(text, :green, :on_blue, :bold)
+    end
+
+    def alert(text)
+      puts @pastel.decorate(text, :white, :on_red, :bold)
+    end
+
   end
-
-  def title(text)
-    puts @pastel.decorate(text, :green, :on_blue, :bold)
-  end
-
-  def alert(text)
-    puts @pastel.decorate(text, :white, :on_red, :bold)
-  end
-
 end
