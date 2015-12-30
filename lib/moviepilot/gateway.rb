19b7c7648ab2e5e3bfaca234cee455cf1ba60e2b
Luis Edimerchk Laverde <edyhack@gmail.com>
2015-12-29 21:35:38 -0500

    DRY a bit the Gateway

--- lib/moviepilot/gateway.rb
+++ lib/moviepilot/gateway.rb
@@ -1,16 +1,23 @@
 module Moviepilot
   class Gateway
+
+    BASE_PATH = "http://api.moviepilot.com/v4"
+
     def self.trending_articles(tag)
-      uri = "http://api.moviepilot.com/v4/tags/#{tag}/trending?per_page=4"
-      response = RestClient.get(uri)
-      JSON.parse(response)['collection']
+      uri = "#{BASE_PATH}/tags/#{tag}/trending?per_page=4"
+      result = get_request(uri)
+      result['collection']
     end
 
     def self.get_article(type, id)
-      uri = "http://api.moviepilot.com/v4/#{type}s/#{id}"
-      response = RestClient.get(uri)
-      article = JSON.parse(response)
+      uri = "#{BASE_PATH}/#{type}s/#{id}"
+      article = get_request(uri)
       { body: article['html_body'], title: article['title'] }
+    end
+
+     def self.get_request(uri)
+      response = RestClient.get(uri)
+      JSON.parse(response)
     end
   end
 end
